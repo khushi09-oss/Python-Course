35eb8c3aa95da83031c00606690bf72c6359c94c
khushi09-oss <sahnikhushi09@gmail.com>
2025-05-07 20:45:04 +0530

    DAY6

---
+++ DAY5.py
@@ -0,0 +1,53 @@
+#DICTIONARY
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+print(thisdict["brand"])
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 1964
+}
+print(thisdict)
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+
+#to get dictionary keys
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = thisdict.keys()
+
+print(x)
+
+#to get dictionary values
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = thisdict.values()
+
+print(x)
+#changing values in  DICTIONARY
+thisdict = {"brand": "Ford","model": "Mustang","year": 1964
+}
+thisdict["year"] = 2018
+            #Superkey   #values
+myfamily = {"child1" : {"name" : "Emil","year" : 2004}, "child2" : { "name" : "Tobias","year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+a=33
+b=200
+if a>b:
+    pass
